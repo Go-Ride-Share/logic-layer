dbe7eaee0fc3bb88395e0cdfda458cb3a4e93ede
Eddie Butkaliuk <eddiebu.1515@gmail.com>
2024-09-26 18:56:54 -0500

    update RequestObjects.cs to better reflect user creation payload

--- AccountManager/RequestObjects.cs
+++ AccountManager/RequestObjects.cs
@@ -8,19 +8,15 @@ namespace GoRideShare
         public string PasswordHash { get; set; } = passwordHash;
     }
 
-    public class UserInfo(Guid uid, string email, string passwordHash, string name,
-                    string bio, string preferences, string phoneNumber, int numberOfRaitings,
-                    double raitingAverage, string photo)
+    public class RegistrationUserInfo(string email, string passwordHash, string name,
+                    string bio, string preferences, string phoneNumber, string photo)
     {
-        public Guid Uid { get; set; } = uid;
         public string Email { get; set; } = email;
         public string PasswordHash { get; set; } = passwordHash;
         public string Name { get; set; } = name;
         public string Bio { get; set; } = bio;
         public string Preferences { get; set; } = preferences;
         public string PhoneNumber { get; set; } = phoneNumber;
-        public int NumberOfRaitings { get; set; } = numberOfRaitings;
-        public double RaitingAverage { get; set; } = raitingAverage;
         public string Photo { get; set; } = photo;
     }
 
