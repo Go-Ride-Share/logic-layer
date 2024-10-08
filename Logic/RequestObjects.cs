f716b506f95b521f1a756b16558f403c03385b0e
Ben Clark <106894338+BenClark43@users.noreply.github.com>
2024-10-08 10:50:10 -0500

    Add SavePost endpoint (#14)
    - Save Endpoint Added to logic Layer
    - Add .yaml documentation

---
+++ Logic/RequestObjects.cs
@@ -0,0 +1,76 @@
+using System.Configuration;
+using System.Text.Json.Serialization;
+
+namespace GoRideShare
+{
+    public class DbLayerResponse
+    {
+        [JsonPropertyName("post_id")]
+        public string? PostId { get; set; }
+    }
+
+    public class PostDetails
+    {
+        [JsonPropertyName("postId")]
+        public string PostId { get; set; }
+        
+        [JsonPropertyName("posterId")]
+        public string PosterId { get; set; }
+
+        [JsonPropertyName("name")]
+        public required string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public required string Description { get; set; }
+
+        [JsonPropertyName("departureDate")]
+        public required string DepartureDate { get; set; }
+
+        [JsonPropertyName("originLat")]
+        public required float OriginLat { get; set; }
+
+        [JsonPropertyName("originLng")]
+        public required float OriginLng { get; set; }
+
+        [JsonPropertyName("destinationLat")]
+        public required float DestinationLat { get; set; }
+
+        [JsonPropertyName("destinationLng")]
+        public required float DestinationLng { get; set; }
+
+        [JsonPropertyName("price")]
+        public required float Price { get; set; }
+
+        [JsonPropertyName("seatsAvailable")]
+        public required int SeatsAvailable { get; set; }
+
+        public PostDetails() { }
+
+        public PostDetails(
+            string postId,
+            string posterId,
+            string name,
+            string description,
+            string departureDate,
+            float originLat,
+            float originLng,
+            float destinationLat,
+            float destinationLng,
+            float price,
+            int seatsAvailable)
+        {
+            PostId = postId;
+            PosterId = posterId;
+            Name = name;
+            Description = description;
+            DepartureDate = departureDate;
+            OriginLat = originLat;
+            OriginLng = originLng;
+            DestinationLat = destinationLat;
+            DestinationLng = destinationLng;
+            Price = price;
+            SeatsAvailable = seatsAvailable;
+        }
+    }
+
+}
