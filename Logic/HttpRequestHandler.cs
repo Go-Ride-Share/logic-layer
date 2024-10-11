396aad54de9127a322ea5e81d2ee7930d3b7e86a
Ben Clark <Clarkb2@myumanitoba.ca>
2024-10-10 21:21:07 -0500

    Refactor GetPosts - Refactor GetPosts for testing with Dependancy Injection

---
+++ Logic/HttpRequestHandler.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http.Headers;
+
+namespace GoRideShare
+{
+    public class HttpRequestHandler : IHttpRequestHandler
+    {
+
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task<(bool, string)> MakeHttpGetRequest(string endpoint, string dbToken, string userId)
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, endpoint){};
+            requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", dbToken);
+            requestMessage.Headers.Add("X-User-ID", userId);
+
+            // Call the backend API to verify the login credentials
+
+            try
+            {
+                var dbLayerResponse = await _httpClient.SendAsync(requestMessage);
+
+                if (dbLayerResponse.IsSuccessStatusCode)
+                {
+                    try
+                    {
+                        var dbResponseContent = await dbLayerResponse.Content.ReadAsStringAsync();
+
+                        return (false, dbResponseContent);
+                    }
+                    catch (Exception ex)
+                    {
+                        return (true, ex.Message);
+                    }
+                }
+                else
+                {
+                    var errorMessage = await dbLayerResponse.Content.ReadAsStringAsync();
+                    return (true, errorMessage);
+                }
+            }
+            catch (System.Net.Http.HttpRequestException ex)
+            {
+                return (true, ex.Message);
+            }
+        }
+    }
+
+}
