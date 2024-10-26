0d20ebe0a960e3be33af278a3936eb433d453ddf
Dhairya Prajapati <dhairyhp15@gmail.com>
2024-10-26 16:44:02 -0500

    Added code to return all posts to view posts without logging in.

--- AccountManager/Program.cs
+++ AccountManager/Program.cs
@@ -1,12 +1,14 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using GoRideShare;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
     .ConfigureServices(services => {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.AddScoped<IHttpRequestHandler, HttpRequestHandler>();
     })
     .Build();
 
